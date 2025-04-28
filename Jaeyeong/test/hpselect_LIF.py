32e2d653bc34fcfb88a3289c11a11724c1f27b0a
JaeyeongHan <jyhan0703@naver.com>
2025-04-28 17:55:35 +0900

    LIF Model Test

---
+++ Jaeyeong/test/hpselect_LIF.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+#Python 3 스타일의 print 함수 사용가능.
+import numpy as np
+import matplotlib.pyplot as plt
+
+thresh = 0.2
+delta_t = 2**(-10)
+tau1 = 5e-3
+decay1 = np.exp(-delta_t/tau1)
+
+mem = 0
+mem_list = list([])
+spike = 0
+x_list = list([])
+spike_list = list([])
+
+def act_fun(thresh, mem):
+	if mem > thresh:
+		return 1
+        #return torch.tensor(1)
+	else:
+		return 0
+        #return torch.tensor(0)
+
+def mem_update(x, mem, spike):
+    mem = mem * decay1 * (1. - spike) + 0.1 * x
+    spike = act_fun(thresh, mem)
+    return mem, spike
+
+import time
+import sys
+
+T = 100
+for step in range(T):
+  if step % 3 == 0:
+    x = 1
+  elif (step % 2 == 0) and (step > 60):
+    x = 1
+  else:
+    x = 0
+  mem, mem1, mem2, spike = mem_update(x, mem, spike)
+  print(f"x: {x}, mem: {mem}, spike: {spike}")
+  mem_list.append(mem)
+  #y_list.append(y_select(spike))
+  x_list.append(x)
+  spike_list.append(spike)
+
+
+plt.subplot(3, 1, 1)
+plt.title("SRM Model", loc = 'left')
+plt.ylabel("Membrane Potential")
+plt.xlabel("Time")
+plt.axhline(y=float(thresh), color='g', linestyle='--')
+plt.plot(mem_list, marker = 'o', ms = 2)
+
+plt.subplot(3, 1, 2)
+plt.ylim(0.9, 1.1)
+plt.ylabel("Input Spike")
+plt.xlabel("Time")
+plt.plot(x_list, '|', color='k')
+
+plt.subplot(3, 1, 3)
+plt.ylim(0.9, 1.1)
+plt.ylabel("Output Spike")
+plt.xlabel("Time")
+plt.plot(spike_list, '|', color='k')
+
+plt.show()
+
+mem_list.clear()
+x_list.clear()
+spike_list.clear()
