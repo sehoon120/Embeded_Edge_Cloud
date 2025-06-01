34b0203ff71e9402a84ba8303d1d55a426557d20
JaeyeongHan <jyhan0703@naver.com>
2025-06-02 01:51:54 +0900

    Update mqtt_setting.sh

--- Jaeyeong/server/test/mqtt_setting.sh
+++ Jaeyeong/server/test/mqtt_setting.sh
@@ -1,2 +1,10 @@
 sudo systemctl start mosquitto
 sudo netstat -tulpn | grep 1883
+
+
+sudo nano /etc/mosquitto/mosquitto.conf
+#bind_address 127.0.0.1 <- erase it
+listener 1883  <- upload
+allow_anonymous true <- upload 
+
+sudo systemctl restart mosquitto
